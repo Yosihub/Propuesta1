241ee3179edaeca686fe62e2a1e909fcdc592e6d
Yosihub <ykathalina4@gmail.com>
2024-08-29 23:32:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.getElementById('no-btn').addEventListener('click', function() {
     document.getElementById('result-img').src = 'caricatura-llorando.png';
     document.getElementById('result').classList.remove('hidden');
 });
+
+const noBtn = document.getElementById('no-btn');
+
+// Añade la clase de animación cuando el mouse está sobre el botón
+noBtn.addEventListener('mouseover', function() {
+    noBtn.classList.add('moving');
+});
+
+// Quita la clase de animación cuando el mouse sale del botón
+noBtn.addEventListener('mouseout', function() {
+    noBtn.classList.remove('moving');
+});
